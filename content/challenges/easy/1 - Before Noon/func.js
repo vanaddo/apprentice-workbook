37aa45b66f5f16a14389ba4293b8f1998f1950bb
vanaddo <samea@samanthas-mbp.myfiosgateway.com>
2021-08-13 11:38:28 -0400

    enter

--- content/challenges/easy/1 - Before Noon/func.js
+++ content/challenges/easy/1 - Before Noon/func.js
@@ -1,4 +1,9 @@
 // Check if the time is before noon
 // time is in military time 0-24
 module.exports = (hour) => {
+    if(hour < 1200){
+        console.log(`${hour} is before noon.`);
+    }else{
+        console.log(`${hour} is afternoon.`);
+    }
 }
