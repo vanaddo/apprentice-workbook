994d6001d55be18ccdfca86a8b992f1963104bcd
vanaddo <samea@samanthas-mbp.myfiosgateway.com>
2021-08-13 12:49:46 -0400

    E:2 Answer

--- content/challenges/easy/2 - Life Stages/func.js
+++ content/challenges/easy/2 - Life Stages/func.js
@@ -7,4 +7,15 @@
 // Greater than 19 and less than 30 is 'Young Adult'
 // Greater than 30 is 'Dead'
 module.exports = (age) => {
+    if(age < 2){
+        return 'Baby';
+    } else if (age > 2 && age < 13) {
+        return 'Kid';
+    } else if (age > 13 && age < 19) {
+        return 'Teen';
+    } else if (age > 19 && age < 30) {
+        return 'Young Adult';
+    } else if (age > 30){
+        return 'Dead';
+    } 
 }
