547b867be4dd7b08ebd3fcfc60013fe13782d437
vanaddo <samea@samanthas-mbp.myfiosgateway.com>
2021-08-13 15:47:26 -0400

    E:6 Answer

--- content/challenges/easy/6 - Flip the Boolean/func.js
+++ content/challenges/easy/6 - Flip the Boolean/func.js
@@ -1,3 +1,10 @@
 // Create a function that reverses a boolean value and returns the string "boolean expected" if another variable type is given
 module.exports = (bool) => {
+    if(bool === true){
+        return false;
+    } else if (bool === false) {
+        return true;
+    } else {
+        return 'boolean expected';
+    }
 };
